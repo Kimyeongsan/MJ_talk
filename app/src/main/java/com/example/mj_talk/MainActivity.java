7f1075113e4b4cb0a3b916a6b0818796cb5fe1d0
qkrthdus605 <qkrthdus605@gmail.com>
2020-07-30 20:31:27 +0900

    Scheduler 추가

--- app/src/main/java/com/example/mj_talk/MainActivity.java
+++ app/src/main/java/com/example/mj_talk/MainActivity.java
@@ -19,8 +19,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
+        getSupportActionBar().hide(); // 액션바를 감추는 추가적인 코드
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_chat)
                 .build();
