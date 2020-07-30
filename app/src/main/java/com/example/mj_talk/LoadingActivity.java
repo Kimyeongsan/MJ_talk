b2b3ab7ceafd171592e3a8674e0398011270fce2
baebang <wowjd2524@gmail.com>
2020-07-30 18:14:27 +0900

    LoadingFrame.add

---
+++ app/src/main/java/com/example/mj_talk/LoadingActivity.java
@@ -0,0 +1,27 @@
+package com.example.mj_talk;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        getSupportActionBar().hide(); // 액션바를 감추는 추가적인 코드
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(LoadingActivity.this,MainActivity.class));
+                finish();
+            }
+        }, 2000); // 2초간 출력
+    }
+}
