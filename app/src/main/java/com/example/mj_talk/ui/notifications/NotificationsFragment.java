50fe4ea6c8cbc7e17bb0a5b1cb422f9cd7127a39
Jeong-gahui <zhengjiaxi0314@gmail.com>
2020-08-07 12:33:10 +0900

    password change

--- app/src/main/java/com/example/mj_talk/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/mj_talk/ui/notifications/NotificationsFragment.java
@@ -1,9 +1,12 @@
 package com.example.mj_talk.ui.notifications;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,6 +15,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.ebanx.swipebtn.SwipeButton;
 import com.example.mj_talk.R;
 
 public class NotificationsFragment extends Fragment {
@@ -23,6 +27,7 @@ public class NotificationsFragment extends Fragment {
         notificationsViewModel =
                 ViewModelProviders.of(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+
 //        final TextView textView = root.findViewById(R.id.text_notifications);
 //        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
 //            @Override
