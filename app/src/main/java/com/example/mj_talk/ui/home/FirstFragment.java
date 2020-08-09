27b82dfd5c26d62dce5f5f42dc0f0052f393f8fe
qkrthdus605 <qkrthdus605@gmail.com>
2020-08-10 00:58:16 +0900

    임시 저장

---
+++ app/src/main/java/com/example/mj_talk/ui/home/FirstFragment.java
@@ -0,0 +1,9 @@
+package com.example.mj_talk.ui.home;
+
+//시간표 Fragment
+//public class FirstFragment extends Fragment {
+//    private View view;
+//
+//    onCreateView();
+//}
+//
---
+++ app/src/main/java/com/example/mj_talk/ui/home/SecondFragment.java
@@ -0,0 +1,5 @@
+package com.example.mj_talk.ui.home;
+
+//달력 Fragment
+public class SecondFragment {
+}
