f153e4481d853cd8141d7c80ff0e55eaf1b75969
Jeong-gahui <zhengjiaxi0314@gmail.com>
2020-08-14 21:38:27 +0900

    //알림 설정

--- app/src/main/java/com/example/mj_talk/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/mj_talk/ui/notifications/NotificationsFragment.java
@@ -1,7 +1,10 @@
 package com.example.mj_talk.ui.notifications;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +13,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -22,7 +26,10 @@ public class NotificationsFragment extends Fragment {
 
     private NotificationsViewModel notificationsViewModel;
     private Button btn_logout;
-
+    private Button swbtn1;
+    private Button swbtn2;
+    private Button swbtn3;
+    private Button swbtn4;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         notificationsViewModel =
@@ -38,6 +45,40 @@ public class NotificationsFragment extends Fragment {
         });
 
 
+
+        Button swbtn1 = root.findViewById(R.id.swbtn1);
+        Button swbtn2 = root.findViewById(R.id.swbtn2);
+        Button swbtn3 = root.findViewById(R.id.swbtn3);
+        Button swbtn4 = root.findViewById(R.id.swbtn4);
+
+     //  Vibrator vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+    //이부분이 오류 떠서... 코멘트 처리했어요ㅜㅜ
+        swbtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+       //          Vibrator.vibrate(VibrationEffect.createOneShot(1000,50);
+        }
+        });
+        swbtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+        //        Vibrator.vibrate(VibrationEffect.createOneShot(1000,60);
+            }
+        });
+        swbtn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+        //        Vibrator.vibrate(VibrationEffect.createOneShot(1000,70);
+            }
+        });
+        swbtn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+        //        Vibrator.vibrate(VibrationEffect.createOneShot(1000,80);
+            }
+        });
+
+
         Button btn_logout = root.findViewById(R.id.button_logout);
         btn_logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,8 +90,6 @@ public class NotificationsFragment extends Fragment {
             }
 
         });
-
-        // 이 프레그멘트 가희가 만든거 !?????
 
         return root;
     }
